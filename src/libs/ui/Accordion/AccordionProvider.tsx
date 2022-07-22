139d158f3179fc8be2b520f2e175df8966503435
rldnrl <devpark1002@gmail.com>
2022-07-22 22:40:13 +0900

    feat: implement accordion provider

---
+++ src/libs/ui/Accordion/AccordionProvider.tsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+export type AccordionContextType = {
+  activeItem: string;
+  changeSelectedItem: (item: string) => void;
+};
+
+const AccordionContext = createContext<AccordionContextType>({
+  activeItem: "",
+  changeSelectedItem: () => {},
+});
+
+export const useAccordionContext = () => {
+  const context = useContext(AccordionContext);
+  if (!context) {
+    throw new Error("Error in creating the context");
+  }
+  return context;
+};
+
+const AccordionProvider = AccordionContext.Provider;
+
+export default AccordionProvider;
