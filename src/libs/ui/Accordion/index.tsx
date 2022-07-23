52400dad7661a42642e60150fae8ef0395373ef2
rldnrl <devpark1002@gmail.com>
2022-07-23 19:02:09 +0900

    feat: Accordion export file

---
+++ src/libs/ui/Accordion/index.tsx
@@ -0,0 +1,8 @@
+export {
+  default as AccordionContext,
+  useAccordionContext,
+} from "./AccordionContext";
+export { default as Accordion } from "./Accordion";
+export { default as AccordionItem } from "./AccordionItem";
+export { default as AccordionButton } from "./AccordionButton";
+export { default as AccordionPanel } from "./AccordionPanel";
