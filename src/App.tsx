54a797550441e154d0e77624fd95baf627336730
rldnrl <devpark1002@gmail.com>
2022-07-23 19:02:21 +0900

    feat: demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+} from "./libs/ui/Accordion";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion>
+        <AccordionItem label="react">
+          <AccordionButton>React</AccordionButton>
+          <AccordionPanel>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem label="angular">
+          <AccordionButton>Angular</AccordionButton>
+          <AccordionPanel>
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem label="javascipt">
+          <AccordionButton>Javasciprt</AccordionButton>
+          <AccordionPanel>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
